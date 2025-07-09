eed60b57de15d3df59fb8eed0e2c8be309e10a1e
Andrew Morris <voltrevo@gmail.com>
2025-07-09 12:45:58 +1000

    Result page improvements

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import calculateSameRange from "./calculateSameRange";
 import Ctx from "./Ctx";
 import never from "./never";
@@ -7,6 +8,16 @@ export default function Result() {
   const result = ctx.result.use();
   const comp = ctx.comp.use();
   const tolerancePct = ctx.tolerancePct.use();
+  const [showDetail, setShowDetail] = useState(false);
+
+  useEffect(() => {
+    const listener = () => setShowDetail(false);
+    window.addEventListener("mouseup", listener);
+
+    return () => {
+      window.removeEventListener("mouseup", listener);
+    };
+  }, [setShowDetail]);
 
   if (result === undefined) {
     return <>Error: result undefined</>;
@@ -22,20 +33,20 @@ export default function Result() {
   const sameRange = calculateSameRange(comp, tolerancePct);
 
   switch (result) {
-    case 'less': {
-      msg = 'You earn less.';
+    case "less": {
+      msg = "You earn less.";
       detail = `They earn ${sameRange.high + 1} or more.`;
       break;
     }
 
-    case 'same': {
-      msg = 'You earn about the same.';
-      detail = `Their compensation is ${sameRange.low} - ${sameRange.high}.`
+    case "same": {
+      msg = "You earn about the same.";
+      detail = `They earn ${sameRange.low} - ${sameRange.high}.`;
       break;
     }
 
-    case 'more': {
-      msg = 'You earn more.';
+    case "more": {
+      msg = "You earn more.";
       detail = `They earn ${sameRange.low - 1} or less.`;
       break;
     }
@@ -45,13 +56,42 @@ export default function Result() {
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1, gap: '1rem' }}>
-      <div className='title'>Result</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: 1,
+        gap: "1rem",
+      }}
+    >
+      <div className="title">Result</div>
       <div style={{ flexGrow: 1 }} />
-      <div style={{ textAlign: 'center', fontSize: '2rem' }}>{msg}</div>
-      <div style={{ textAlign: 'center' }}>{detail}</div>
+      <div style={{ textAlign: "center", fontSize: "1.5rem" }}>{msg}</div>
+      <a
+        onMouseDown={() => setShowDetail(true)}
+        style={{ textAlign: "center", userSelect: "none" }}
+      >
+        Hold to see their implied range.
+      </a>
+      <div
+        style={{
+          textAlign: "center",
+          border: "2px dashed black",
+          padding: "0.5rem",
+        }}
+      >
+        <div style={{ visibility: showDetail ? "visible" : "hidden" }}>
+          <div>{detail}</div>
+          <div style={{ fontSize: "0.7rem", textWrap: "balance" }}>
+            <span style={{ color: "red" }}>Danger</span>: Sharing this would
+            reveal your <i>exact</i> compensation.
+          </div>
+        </div>
+      </div>
       <div style={{ flexGrow: 2 }} />
-      <button style={{ width: '100%' }} onClick={() => ctx.reset()}>Home</button>
+      <button style={{ width: "100%" }} onClick={() => ctx.reset()}>
+        Home
+      </button>
     </div>
   );
 }
